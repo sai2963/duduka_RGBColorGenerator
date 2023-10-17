192729a95f0c76e877576a2c8b818378df3da5ad
sai2963 <venkatsai0629@gmail.com>
2023-10-17 00:07:47 -0700

    file updated

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ let button1 = document.getElementById("btn1");
 rgb.textContent = "rgb(121,25,36)";
 
 function colorpicker() {
-    greenvalue.textContent='';
-    bluevalue.textContent='';
-
+    
     let input1 = redslider.value;
     redvalue.textContent = input1;
    
@@ -23,5 +21,42 @@ function colorpicker() {
   
     let input3 = blueslider.value;
     bluevalue.textContent = input3;
+
+    let rgb1="RGB ("+input1+","+input2+","+input3+")";
+    let rgb2=`rgb(${input1},${input2},${input3})`
+    rgb.textContent=rgb1;
+    colorbox.style.backgroundColor=rgb2;
+
 }
-slider.addEventListener("input", colorpicker);
+colorpicker();
+redslider.addEventListener("input", colorpicker);
+
+
+greenslider.addEventListener("input", colorpicker);
+
+
+blueslider.addEventListener("input", colorpicker);
+
+function copy(){
+    let input1 = redslider.value;
+    redvalue.textContent = input1;
+   
+    let input2 = greenslider.value;
+    greenvalue.textContent = input2;
+  
+    let input3 = blueslider.value;
+    bluevalue.textContent = input3;
+
+    let rgb1="RGB ("+input1+","+input2+","+input3+")";
+    let rgb2=`rgb(${input1},${input2},${input3})`
+    navigator.clipboard.writeText(rgb2)
+        .then(()=>{
+            alert("RGB Color Copied"+rgb2);
+        })
+        .catch((error)=>{
+            console.error("Failed to copy RGB Color:",error)
+        })
+    
+}
+
+button1.addEventListener('click',copy)
